6288a31e2f3647844d02d6ceb9e827be81a40681
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-07 13:46:51 -0400

    finished lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+    Song.create(name: Faker::Educator.course_name, artist_id: rand(1..10), genre_id: rand(1..10))
+    Artist.create(name: Faker::Music.band, bio: "No Bio Present")
+    Genre.create(name: Faker::Music.genre)
+end
